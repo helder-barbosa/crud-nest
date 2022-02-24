3edc47cfbd181acf851308020d6f1031efce642c
Helder Barbosa <helderbarbosa.dev@gmail.com>
2022-02-23 23:22:23 -0300

    #4 Global module

---
+++ src/database/database.module.ts
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+import { Module, Global, DynamicModule } from '@nestjs/common'
+import { MySQLProvider } from './mysql.provider'
+
+@Global()
+@Module({})
+export class DatabaseModule {
+  static forRoot(): DynamicModule {
+    const connectionFactory = {
+      provide: 'DATABASE',
+      useFactory: () => {
+        return new MySQLProvider()
+      }
+    }
+    return {
+      module: DatabaseModule,
+      providers: [connectionFactory],
+      exports: ['DATABASE']
+    }
+  }
+}
--- src/database/mysql.provider.ts
+++ src/database/mysql.provider.ts
@@ -1,14 +1,27 @@
 /* eslint-disable prettier/prettier */
 import { Injectable, Logger } from '@nestjs/common'
+import { createPool, Pool } from 'mysql2/promise'
 
 @Injectable()
 export class MySQLProvider {
   private readonly logger: Logger
+  private readonly pool: Pool
   constructor() {
     this.logger = new Logger('MySQLProvider')
+    this.pool = createPool({
+      host: 'localhost',
+      user: 'root',
+      database: 'cat-products',
+      waitForConnections: true,
+      connectionLimit: 20,
+      queueLimit: 0
+    })
     this.logger.log('Initialized !')
   }
-  getValue(): string {
+  async getValue(): Promise<string> {
+    const conn = await this.pool.getConnection()
+    const [results] = await conn.query('select * from products')
+    console.log(results)
     return 'Value from MySql Provider !'
   }
 }
